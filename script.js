47a77e366f8797dca992eb3d0c98eaf92a7fa537
Abner <tomaz.abner@escola.pr.gov.br>
2026-04-30 12:05:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function pagina() {
     fetch('dados.json')
         .then(response => response.json())
-        .then(dadinho => {
-            document.getElementById('destino').innerHTML =
+        .then(ddd => {
+             tablet = document.getElementById('destino');
+
+             ddd.forEach(dadinho => {
+                let linha = document.createElement('li');
+
+                linha.innerHTML =
                 `<div class="card">
-                <img src="img/`+ dadinho.imagem + `" class="imagem_produto" alt="">
-                <h3>`+ dadinho.nome + `</h3>
-                <p class="d_produto">`+ dadinho.descricao + `</p>
-                <p class="p_produto">R$ `+ dadinho.preco + `</p>
-                <p class="botao_card"><a class="letra_botao" href="`+ dadinho.endereco + `">Saiba Mais</a></p>
-            </div>`;
+                    <img src="img/`+ dadinho.imagem + `" class="imagem_produto" alt="">
+                    <h3>`+ dadinho.nome + `</h3>
+                    <p class="d_produto">`+ dadinho.descricao + `</p>
+                    <p class="p_produto">R$ `+ dadinho.preco + `</p>
+                    <p class="botao_card">
+                        <a class="letra_botao" href="`+ dadinho.endereco + `">Saiba Mais</a>
+                    </p>
+                </div>`;
+            tablet.appendChild(linha);
+            });
+                
         })
 }
